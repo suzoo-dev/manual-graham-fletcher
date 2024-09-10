0b1b842338f3e95cf05dbdfcdfd9800aaebf885a
suzoo-dev <57764195+suzoo-dev@users.noreply.github.com>
2024-09-10 01:57:25 +0100

    Hair Loss and ED sections added

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,15 @@ import Image from "next/image";
 import Link from "next/link";
 import { ttNormsMed } from "./layout";
 import logo from "../public/Symbol.png";
+import hairImage from "../public/Hair-Photo.png";
+import edImage from "../public/ED-Photo.png";
+
+const paragraphText = () => (
+  <p className={`mb-8 text-lg leading-8 text-primary ${ttNormsMed.className}`}>
+    {`We're working around the clock to bring you a holistic approach to
+    your wellness. From top to bottom, inside and out.`}
+  </p>
+);
 
 export default async function Home() {
   const response = await fetch(
@@ -26,23 +35,54 @@ export default async function Home() {
             >
               Be good to yourself
             </h1>
-            <p
-              className={`mb-8 text-lg leading-8 text-primary ${ttNormsMed.className}`}
-            >
-              We’re working around the clock to bring you a holistic approach to
-              your wellness. From top to bottom, inside and out.
-            </p>
+            {paragraphText()}
             <div>
               <Link
                 href="/quiz"
                 replace
-                className={`text-xs leading-8 text-white bg-[#7E0707] px-8 py-4 ${ttNormsMed.className}`}
+                className={`text-xs leading-8 text-white bg-[#7E0707] px-8 py-4 tracking-widest ${ttNormsMed.className}`}
               >
                 TAKE THE QUIZ
               </Link>
             </div>
           </div>
           <div></div>
+        </div>
+      </div>
+      {/* Hair */}
+      <div className="flex flex-col w-full">
+        <h1
+          className={`flex items-center text-center justify-center text-4xl text-primary h-16 w-full mt-16 mb-16 ${ttNormsMed.className}`}
+        >
+          What we can help with
+        </h1>
+        <div className="flex flex-row justify-around mb-20">
+          <div className="ml-28">
+            <Image src={hairImage} alt="Male head showing hair" />
+          </div>
+          <div className="w-[372px] mr-28">
+            <h4 className={`text-xs leading-10 ${ttNormsMed.className}`}>
+              HAIR LOSS
+            </h4>
+            <h2
+              className={`text-3xl ${ttNormsMed.className} mb-4`}
+            >{`Hair loss needn't be irreversible. We can help!`}</h2>
+            {paragraphText()}
+          </div>
+        </div>
+        <div className="flex flex-row justify-around mb-20">
+          <div className="w-[372px] ml-28">
+            <h4 className={`text-xs leading-10 ${ttNormsMed.className}`}>
+              ERECTILE DYSFUNCTION
+            </h4>
+            <h2 className={`text-3xl ${ttNormsMed.className} mb-4`}>
+              Erections can be a tricky thing. But no need to feel down!
+            </h2>
+            {paragraphText()}
+          </div>
+          <div className="mr-28">
+            <Image src={edImage} alt="Male head shot" />
+          </div>
         </div>
       </div>
     </div>
